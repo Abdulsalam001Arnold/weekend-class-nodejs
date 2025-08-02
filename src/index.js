1b4612b413233cecf9ff05f0d9127cb120b62bf2
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-08-02 14:45:58 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -11,10 +11,19 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database connect
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+    res.send('This is our hosted API!!!')
+})
 app.use('/api', userRoute)
 app.use(basicRoute)
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+
+if(process.env.NODE_ENV !== "production"){
+    const PORT = 3000
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
+
+module.exports = app
